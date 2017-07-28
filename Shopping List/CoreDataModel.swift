090983160a8437dd2e2d75c477048d14e55cf6b9
Joe Lucero <jlucerond@gmail.com>
2017-07-28 09:48:55 -0600

    Initial Commit

---
+++ Shopping List/CoreDataModel.swift
@@ -0,0 +1,27 @@
+//
+//  CoreDataModel.swift
+//  Shopping List
+//
+//  Created by Joe Lucero on 7/28/17.
+//  Copyright © 2017 Joe Lucero. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        let appName = Bundle.main.object(forInfoDictionaryKey: kCFBundleNameKey as String) as! String
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error): \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+    
+}
---
+++ Shopping List/ShoppingListTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingListTableViewController.swift
+//  Shopping List
+//
+//  Created by Joe Lucero on 7/28/17.
+//  Copyright © 2017 Joe Lucero. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListTableViewController: UITableViewController {
+
+    
+
+}
