b76fea2652a80f831784436cc55eda0924627d76
Joe Lucero <jlucerond@gmail.com>
2017-07-28 11:42:31 -0600

    Project Completed
    Basic structure of app with MVC & Core Data principles in place.

---
+++ Shopping List/GroceryItemModelController.swift
@@ -0,0 +1,36 @@
+//
+//  GroceryItemModelController.swift
+//  Shopping List
+//
+//  Created by Joe Lucero on 7/28/17.
+//  Copyright © 2017 Joe Lucero. All rights reserved.
+//
+
+import Foundation
+
+class GroceryItemModelController {
+    static let shared = GroceryItemModelController()
+    
+    func addNewGroceryItem(name: String) {
+        let _ = GroceryItem(name: name)
+        saveToCoreData()
+    }
+    
+    func delete(groceryItem: GroceryItem) {
+        groceryItem.managedObjectContext?.delete(groceryItem)
+        saveToCoreData()
+    }
+    
+    func changeToggleOn(groceryItem: GroceryItem) {
+        groceryItem.isPurchased = !groceryItem.isPurchased
+        saveToCoreData()
+    }
+    
+    func saveToCoreData() {
+        do {
+            try CoreDataStack.context.save()
+        } catch {
+            print("Error while saving.")
+        }
+    }
+}
